142ccc62cd5fdb8e4c5a2a8dc44af8cc8e52b636
JeLe <dev.lecarpentier@gmail.com>
2014-03-24 13:39:08 +0100

    mgvimzgvmgvmogzrougf
    ce commit il fait plein de trucs trop coollllllls !

--- project.py
+++ project.py
@@ -18,15 +18,15 @@ from math import sin,cos,sqrt,pi
 ESCAPE = '\033'
 # Number of the glut window.
 window = 0
-alpha = 0
+alpha = -90
 transz = 0
-xold=600
+xold = 600
 
-
-forward =0
+forward = 0
 direction = 0
 
-
+walls = []
+doors = []
 ########################################
 #this is where you put your dude classes
 
@@ -48,6 +48,7 @@ class foot (object):
                                [x+3*unite, y , z], [x+3*unite, y+3*unite , z], [x+3*unite, y+3*unite , z+3*unite], [x+3*unite, y, z+3*unite],
                                [x, y+3*unite, z], [x+3*unite, y+3*unite, z], [x+3*unite, y+3*unite, z+3*unite],[x, y+3*unite, z+3*unite]]
 
+
 class jambe (object):
     def __init__ (self, Ax, Ay, Az):
         self.Ax = Ax
@@ -65,6 +66,22 @@ class jambe (object):
                                 [x, y, z+unite], [x, y+5*unite, z+unite], [x+3*unite, y+5*unite, z+unite], [x+3*unite, y, z+unite]]
 
 
+class torse (object):
+    def __init__ (self, Ax, Ay, Az):
+        self.Ax = Ax
+        self.Ay = Ay
+        self.Az = Az
+    
+    def getPoints(self):
+        unite = .5
+        x = self.Ax
+        y = self.Ay+8*unite
+        z = self.Az
+        self.torseVertexList = [[x, y, z], [x, y+5*unite, z], [x+7*unite, y+5*unite, z], [x+7*unite, y, z],
+                                [x, y, z]]
+                                
+
+
 class bonhomme (object):
     def __init__(self, Ax, Ay, Az):
         self.Ax = Ax
@@ -73,47 +90,58 @@ class bonhomme (object):
         self.type = "moving"
     
     def getPoints(self):
+        unite = .5
         myFoot = foot(self.Ax, self.Ay, self.Az)
         myFoot.getPoints()
-        self.vertexList = myFoot.footVertexList
-    #self.vertexList.append()
+        myFoot2 = foot(self.Ax+4*unite, self.Ay, self.Az)
+        myFoot2.getPoints()
+        myJambe = jambe(self.Ax, self.Ay, self.Az)
+        myJambe.getPoints()
+        myJambe2 = jambe(self.Ax+4*unite, self.Ay, self.Az)
+        myJambe2.getPoints()
+        self.vertexList = []
+        self.vertexList.append(myFoot.footVertexList)
+        self.vertexList.append(myJambe.jambeVertexList)
+        self.vertexList.append(myFoot2.footVertexList)
+        self.vertexList.append(myJambe2.jambeVertexList)
     
     def drawBonhomme(self):
         self.getPoints()
         glBegin(GL_QUADS)
-        for item in self.vertexList:
-            glColor3f(0.7, 0.4, 0.9)
-            glVertex3f(item[0], item[1], item[2])
+        for truc in self.vertexList:
+            for item in truc:
+                glColor3f(0.7, 0.4, 0.9)
+                glVertex3f(item[0], item[1], item[2])
         glEnd()
     
     def move(self):
         global forward
         global direction
         #ok so direction devient le coefi dir de ladroite sur laquelle se deplace le bnhomme, on incremente de ce qu'on veut siur x, on calcule z et voila...
-        #en meme temps, on fait un rotatef de direction, on dessine le monsieur, et on derotatef... comme ca pas besoin de vecteurs ;)
+        #pb : on voit pas quand il tourne...
         if forward != 0:
             b= self.Az-direction*self.Ax
             self.Ax = self.Ax + forward
             self.Az = (direction*self.Ax)+b
             
             forward = 0
-            print (direction)
 
 
-# all my classes here, and then the instanciation of my first objects !
 
 
 class wall (object):
-    def __init__(self):
+    def __init__(self, thingToWall):
         #i don't really need any thing to be in my wall yet...
-        True
-
+        self.getWallVertices(thingToWall)
+        walls.append(self)
+    
     def getWallVertices(self, thingToWall):
         thingToWall.getQuadVertices()
         
         self.vertices = []
         for vertex in thingToWall.vertices:
             self.vertices.append([vertex[0], 0.1, vertex[2]])
+
 #z2-z0, z3-z1
     def checkNotOnWall(self):
         self.equation1 = (self.vertices[2][2]-self.vertices[0][2])/(self.vertices[2][0]-self.vertices[0][0])
@@ -142,8 +170,8 @@ class quad(object):
         self.green = green
         self.blue = blue
         #I ommited the creation of the vertices here because they might need to be regurlarly updated...
-        self.wall = wall()
-        
+        self.wall = wall(self)
+
     def getQuadVertices(self):
         self.vertices = [[self.Ax, self.Ay, self.Az], [self.Ax+self.Vx, self.Ay+self.Vy, self.Az+self.Vz],[self.Ax+self.Vx+self.Wx, self.Ay+self.Vy+self.Wy, self.Az+self.Vz+self.Wz], [self.Ax+self.Wx, self.Ay+self.Wy, self.Az+self.Wz]]
 
@@ -155,7 +183,7 @@ class quad(object):
             glVertex3f(vertex[0], vertex[1], vertex[2])
         glEnd()
 
-        self.wall.getWallVertices(self)
+#self.wall.getWallVertices(self)
         if self.name != "floor":
             glBegin(GL_QUADS)
             glColor3f(0., 1., 0.)
@@ -163,10 +191,13 @@ class quad(object):
                 glVertex3f(vertex[0], vertex[1], vertex[2])
             glEnd()
 
-
+#here is where wa instanciate all the objects....
+#we now need to get the walls for all the machines and ... youpii
 
 floor = quad("floor", -5, 0.0, 5, 0., 0.0, -10., 10., 0.0, 0., 1.0, 1., 0.)
+test = quad("test", -10, 0.0, 0, 10., 0.0, 0., 0, 2., 0., 0., 0.4, 0.7)
 
+print (walls[1].vertices)
 
 def InitGL(Width, Height):
     glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -223,10 +254,10 @@ def DrawGLScene():
 
     #and here is the floor :)
     floor.drawQuad()
+    test.drawQuad()
 
     MyBonhomme.move()
     MyBonhomme.drawBonhomme()
-
     
         
     glutSwapBuffers()
@@ -236,25 +267,13 @@ def DrawGLScene():
 def keyPressed(*args):
     #global alpha
     global transz
+    global alpha
+
     # If escape or q is pressed, kill everything.
     if args[0] == ESCAPE or args[0] == 'q':
         sys.exit()
-    global alpha
-    global direction
-    global forward
-    #to move the dude...
-    if args[0] == 'o':
-        forward = .1
-    if args[0] == 'l':
-        forward = -.1
-    if args[0] == 's':
-        forward = 0
-    if args[0] == 'm':
-        direction += .1
-    if args[0] == 'k':
-        direction += -.1
 
-
+    #manual rotations and zooms
     if args[0] == 'g':
         alpha += 1
     if args[0] == 'd':
@@ -263,8 +282,24 @@ def keyPressed(*args):
         transz += -1.
     if args[0]== 'f':
         transz += 1.
-        
-def myMouseMove ( x, y):
+
+
+def specialKeyPressed(key, x, y):
+    global direction
+    global forward
+    #to move the dude...
+    if key == GLUT_KEY_UP :
+        forward = .1
+    if key == GLUT_KEY_DOWN:
+        forward = -.1
+    if key == GLUT_KEY_LEFT:
+        direction += .1
+    if key == GLUT_KEY_RIGHT:
+        direction += -.1
+
+
+
+def myMouseMove (x, y):
     global alpha
     global xold
     if xold<x:
@@ -288,12 +323,14 @@ def main():
     
     glutDisplayFunc(DrawGLScene)
     
-    #glutFullScreen()
+    #    glutFullScreen()
     
-    # these are the callbacks to the functions that actually do something...
+    #these are the callbacks to the functions that actually do something...
     glutIdleFunc(DrawGLScene)
     glutReshapeFunc(ReSizeGLScene)
     glutKeyboardFunc(keyPressed)
+    glutSpecialFunc(specialKeyPressed);
+
     glutMouseFunc( myMouseMove)
     glutPassiveMotionFunc( myMouseMove)
     
