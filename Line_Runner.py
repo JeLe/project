b4ca3d046895fdcfa914e047b99c4137cb33aeb4
FrenchFries145 <antoinepincon1@gmail.com>
2014-04-21 16:55:23 +0200

    Update Line_Runner.py

--- Line_Runner.py
+++ Line_Runner.py
@@ -4,13 +4,19 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
 from Image import *
-
+from random import *
 
 ESCAPE = '\033'
 
 # variables pour la fenetre du programme 
 window = 0
 alpha =0
+
+#variables pour le score et l'acceleration au cours du jeu
+score=0
+upscore=5
+acceleration=10
+increment=0.1
 
 #creation de l'objet 'player' (ce sera le personnage/le 'truc' qui devra eviter les obstacles dans le jeu)
 class player (object):
@@ -54,6 +60,21 @@ class carre (object):
 monplayer= player (-1.7,-0.9,[1.,1.,0.0])
 moncarre = carre (0.9,0.9,[0.1,0.8,0.5])
 
+#liste faisant apparaitre les premiers obstacles du jeu
+list1=[carre(0.9,0.5,[0.8,0.2,0.1]),carre(0.9,0.1,[.0,.4,.5]),carre(0.9,0.8,[0.1,.8,.6])]
+
+def NewObstacle(counter):
+  x=randint in range(0,0.5)
+  y=randint in range(0,0.5)
+  list1[counter]=carre(x,y,[0.5,0.2,0.7])
+  global score
+  points+=upscore
+  if score>acceleration:
+    increment+=0.05
+    upscore+=2
+    acceleration+=100*0.5*upscore
+    print(score)
+    
 # Fonction d'initialisation d'OpenGL. Defini les parametres principaux.
 def InitGL(Width, Height):			         	# On l'appelle juste apres que la fenetre OpenGL ait ete creee.
     glClearColor(0.2, 0.5, 0.8, 0.0)                            # permet de changer la couleur de fond de la fenetre
@@ -86,17 +107,25 @@ def DrawGLScene():
     monplayer.getvertices()
     monplayer.drawplayer()
     moncarre.drawcarre()
-    
-    if monplayer.Ax < moncarre.Ax < monplayer.Ax+0.3 and (monplayer.Ay<=moncarre.Ay<=monplayer.Ay+0.3 or monplayer.Ay<=moncarre.Ay+0.3<=monplayer.Ay+0.3):
-        sys.exit()
-        
+#condition pour la mort du joueur:
+    if (monplayer.Ax < moncarre.Ax < monplayer.Ax+0.3 or monplayer.Ax<moncarre.Ax+0.5<monplayer.Ax+0.3)  and (monplayer.Ay<=moncarre.Ay<=monplayer.Ay+0.3 or monplayer.Ay<=moncarre.Ay+0.5<=monplayer.Ay+0.3):
+      sys.exit()
+#conditions pour que le joueur reste dans l'ecran
+    if monplayer.Ay>=1.7:
+      monplayer.Ay=1.7
+    if monplayer.Ay<=-2:
+      monplayer.Ay=-2
+#condition pour faire apparaitre les nouveaux obstacles
+    if moncarre.Ax<=monplayer.Ax:
+      counter=0
+      for item in list1:
+	if item.Ax<=-0.5:
+	  item=NewObstacle(counter)
+	  counter+=1
+	    
         ##############################################
         
-    
-    glutSwapBuffers()
-    
-    
-    
+    glutSwapBuffers()   
     
     #on defini ici les touches et les actions correspondantes
 def keyPressed(*args):
@@ -104,24 +133,22 @@ def keyPressed(*args):
     global alpha
     if args[0] == ESCAPE or args[0] == 'q': # Si on appuie sur 'q' ou 'echap', ferme le programme
         sys.exit()
-    if args[0] == 'z':
+    if args[0] == 'z': #si on appuie sur 'z' le personnage se deplace vers le haut
         monplayer.Ay+=0.1
-    if args[0] == 's':
+    if args[0] == 's': #si on appuie sur 's' le personnage se deplace vers le bas
 	monplayer.Ay-=0.1
-
 
 def main():
     global window
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
-    glutInitWindowSize(640, 480)
+    glutInitWindowSize(640, 480) #defini la taille de la fenetre
     glutInitWindowPosition(0, 0)
     window = glutCreateWindow( "Line_Runner")  #donne un nom a la fenetre
     glutDisplayFunc(DrawGLScene)
     glutIdleFunc(DrawGLScene)
     glutReshapeFunc(ReSizeGLScene)
     glutKeyboardFunc(keyPressed)
-    
     InitGL(640, 480)
     glutMainLoop()
 
