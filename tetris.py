09ce55cbcd402619469d60a8d08a4cd98dd114f2
JeLe <dev.lecarpentier@gmail.com>
2014-05-08 16:00:48 -0700

    et voila !

--- tetris.py
+++ tetris.py
@@ -131,9 +131,10 @@ class piece6(object):
         self.x = 4 
         self.y = 12
         self.color =[0,1,0]  # cyan  / anti L
-        self.list = [carre(self.x+1,self.y,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color),carre(self.x,self.y-2,self.color)] 
+        self.list = [carre(self.x+1,self.y,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color),carre(self.x,self.y-2,self.color)]
     
-class piece7(object):
+
+class piece(object):
     def __init__(self):
         self.x = 4 
         self.y = 12
@@ -142,30 +143,44 @@ class piece7(object):
     
     
     #ca c'est pour recuperer ta fameuse liste que tu avais au depart. Sauf que la en plus tu peux mettre les autres listes pour la rotation et faire un truc qui choisit la bonne :) ( LISTE TOUT EN HAUT ? OU DU COUP JPEUX METTRE MES OBJETS PIECES ? )
+    ##Alors en fait, self.list, c'est la vraie liste de tes carres. Tu pourra meme y ajouter les listes de tes differentes configurartions, et comme ca en fait, tu fais la representation de ta piece dans ton code... c'est ca qui est dessine dans la methode draw(). Et non, tu ne peux pas mettre tes objets pieces, parce que jusqua ce que je dirai stop, c'est dans la classe piece, donc ca c'est la liste de cette piece en particulier...
+    ##je crois que ca c'est toujours pas tres clair, alors si t'a des questions dessus n'hesite pas :)
+    
     def getSquareList(self):
-        test = 0 # cette ligne c'est juste pour eviter une erreur, il faut qua tu ecrives ta methode...
-        self.bas #il faut aussi que ici tu mette un parametre a ton objet pour savoir ou est le bas... Comme ca tu compares ce parametre a ce qui representera le bas pour savoir si il est atteint ou pas..
+        ## pour l'instant, on recalcule juste la liste, il faudra plus tard ajouter le choix de la bonne configuration et tout et tout
+        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x+1,self.y-2,self.color)]
+        self.bas = "prout" #il faut aussi que ici tu mette un parametre a ton objet pour savoir ou est le bas... Comme ca tu compares ce parametre a ce qui representera le bas pour savoir si il est atteint ou pas..
     
     #cette methode descend ton objet, il faut l'appeler dans le timer... (JE L'AI APPELE DANS LE MOVE OU YA LE TIMER) ( MAIS VOILA YA DEUX MOVE ..)
+    ##oui c'est ce qu'il fallait faire :)
     def down(self):
-        self.y -= 1 
+        self.y -= 1
 
     #ca c'est pour bouger de gauche a droite. Il faut pourvoir le faire plus d'une fois par timer, donc on l'appelle a chaque fois qu'une touche est enfoncee. c'est a dire dans la fonction Keypressed ! ( LE PERMIER MOVE(SELF) )
-    def move(self):
-        self.y += direct
+    ##j'en ai marre et il fait chaud !!!!!!!!
+    ## mais sinon ca va :)
+    def backAndForth(self):
+        global direct
+        self.x += direct ##il va falloir ajouter une condition pour qu'on ne puisse pas sortir de la zone de jeu...
         direct = 0
 
     def draw(self):
-        test = 0
+
         #alors ici il faut que tu prenne le self.list rafraichit (c'est a dire apres avor appele la methode getSquareList, et que tu fasse une boucle pour appeler la methode draw de chaque element (les instances de la classe carre ) de ta liste.
+        #donc vu qu'il me le faut pour faire des tests je te fais cette boucle, que tu n'aurais eu aucun mal a faire quoi qu'il arrive
+        self.getSquareList()
+        for thing in self.list :
+            thing.draw() #on appelle ici la methode draw de l'objet carre, et on s'en fout si on a 2 draw, parce que ils sont dans 2 classes differentes (la classe carre et la classe piece...)
+
+##LA FIN DE LA ClASSE C'EST ICI !
 
 
-
+##en fait, cette fonction elle est un peu pourrie, et a cause d'elle tu as besoin de 50000 classes de pieces differentes. Alors je l'ai remplacee par un choix random dans la classe piece unique que j'ai refaite a partir des autres classes que toi tu avait. c'est en gros une refonte, un espece de deux en 1 quoi, donc c'est moins cher et c'est plus pratique !
 def randomPiece():
     test = 0 #meme principe : faut que tu te fasse ta fonction ! alors soit tu fait comme t'avais fait, tu fais une liste d'instances de classes pieces (une de chaque) et tu fait ton choice
     #tu peux donc par exemple faire ta liste comme ceci
-    piecerandom = [piece1(), piece2()] #avec piece1 et tout ca etant des classes et entre les () tu mets ce qu'il faut quoi !
-    pieceR = random.choice(piecerandom)     #du coup cette ligne tu peux peut etre la garder :)
+    piecerandom = [piece6(), piece()] #avec piece1 et tout ca etant des classes et entre les () tu mets ce qu'il faut quoi !
+    return( random.choice(piecerandom) )    #du coup cette ligne tu peux peut etre la garder :)
 
 def bas():
     test = 0 #si tu continue a lire mes commentaires ti verras ce que c'est que cette fonction :)
@@ -179,39 +194,24 @@ def move():
     #global y
     #global direct
     #du coup tu n'as plus besoin que de ta variable globale qui est l'objet qui est en train de descendre !
-    global pieceR  #mais tu n'auras pas besoin de cette ligne, car c'est un objet...     (DONC LA JE COMPREND PAS DU COUP, JE SAIS PLUS QUOI GARDER)
+    #global pieceR  #mais tu n'auras pas besoin de cette ligne, car c'est un objet...     (DONC LA JE COMPREND PAS DU COUP, JE SAIS PLUS QUOI GARDER)
     
-    down()
+    ##ce que je voulais dire, c'est qu'en fait pieceR ne va plus etre une variabl globale : ce sera maintenat une instance d'un des objets piece.
+    ## et vu que tu va jouer avec les x et y de cette instance, tu n'a plus besoin de tes 15000 variables globales qui sont en commmentaire audessus !
+    ##voila donc a quoi ca va ressembler :
     
-   
     if pieceR.y>3 :
         #j'ai enleve le truc du x, il est deja apelle dans le keyboardfunc...
         #mais ici on fait descendre la piece
         pieceR.down()
+    
+    ##tout simplement :) Mainetant tout le travail est fait a l'interieur de ta classe piece quelle qu'ell soit !
         
-
+        ## ca je te laisse y reflechir encore un peu :)
     if pieceR.y<=3 : #ca ca veut dire que ta piece est arrivee en bas...
         #alors il va faloir gerer une representation dynamique de ce qu'est le bas. C'est a dire que une fois qu'on a commence a jouer le bas n'est plus juste une ligne...
         #donc moi je te propose de faire une nouvelle fonction par exemple bas(), que j'ai declaree plus haut, et qui va regarder si le pieceR.bas est sur la ligne du bas et si c'est le cas va faire en sorte d'incorporer cette piece qui vient d'atterir dans ce que tu choisira pour representer le bas, soit un objet, soit une liste ou ce que tu veux, mais choisit bien parce que ca va deja etre assez galere comme ca .... :( Du coup les ifs de cette fonction ne servent plus a grand chose, il faudra juste appeler ta nouvelle fonction bas avant de descendre ta piece..
         test = 0#et donc pour l'instant je te mets ma super ligne qui ne sert a rien mais evite une erreur d'indentation a cause du if :
-
-
-
-
-#FABRICATION
-#Grille de jeu
-
-# est ce qu'elle sert vraiment a qqch ta grille ? parce qu'en fait t'es parti avec des pieces independantes et pas des etats de pixels (les carres de la gille...) mais c'est pas grave du tout c'est ta facon de faire et ca marche tout aussi bien :)
-# cela dit laisse la ou elle est pour l'instant... elle ne gene personne !
-
-moncarre=carre(0,0,[1.0,1.0,1.0])
-machin=[]
-grille=[]
-
-for ligne in range(10):
-      for colonne in range(20):
-            machin.append(carre(ligne,colonne,[1.0,1.0,1.0]))
-      grille.append(machin)
 
 
 
@@ -247,9 +247,7 @@ def ReSizeGLScene(Width, Height):
 # Fonction de dessin principale
 
 def DrawGLScene():
-    global piece1,piece2,piece3,piece4,piece5,piece6,piece7,piecerandom,pieceR
-    global x
-    global y
+    ##ici en fait y'avait pas besoin de variables globales...
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)	# Clear The Screen And The Depth Buffer
     glLoadIdentity()	# Reset The View
     glTranslatef(-5.,-7.0,-15.0)	# Move Into The Screen
@@ -269,9 +267,7 @@ def DrawGLScene():
 
 
 def keyPressed(key, x, y):
-    global window
-    global direct, stop
-
+    global direct
     # Si t ou echap presse, tout effacer
     if key == ESCAPE or key == 't':
         sys.exit()
@@ -279,13 +275,11 @@ def keyPressed(key, x, y):
     if key == 'd':
           direct = 1
 
-    if key == 'q':
-          direct = 0
-
     if key == 's':
           direct = -1
-
-    move()
+##    move()
+##c'etait pas move qu'il fallait appeler, mais ce qui est maintenat backAndForth
+    pieceR.backAndForth()
 
 
 def main():
@@ -305,8 +299,29 @@ def main():
     InitGL(640, 480)
     glutMainLoop()
 
+
+
+#FABRICATION
+#Grille de jeu
+
+# est ce qu'elle sert vraiment a qqch ta grille ? parce qu'en fait t'es parti avec des pieces independantes et pas des etats de pixels (les carres de la gille...) mais c'est pas grave du tout c'est ta facon de faire et ca marche tout aussi bien :)
+# cela dit laisse la ou elle est pour l'instant... elle ne gene personne !
+
+moncarre=carre(0,0,[1.0,1.0,1.0])
+machin=[]
+grille=[]
+
+for ligne in range(10):
+    for colonne in range(20):
+        machin.append(carre(ligne,colonne,[1.0,1.0,1.0]))
+    grille.append(machin)
+
+##il faut donc ici, car c'est ici qu'on fabrique tout visiblement, fabriquer pieceR :
+
+
 #alors maintenant avant d'appeler tes fonctions (move() et main()), il faut creer une premiere piece qui descend et qui est choisie aleatoirement :
-randomPiece()
+pieceR = randomPiece()
+
 
 move()
 main()
