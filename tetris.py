6062e0c5708b6d7623571eecb41648798ff93992
JeLe <dev.lecarpentier@gmail.com>
2014-05-09 12:32:20 -0700

    alors la le debut il marche...

--- tetris.py
+++ tetris.py
@@ -74,87 +74,78 @@ window = 0
 
 class carre(object):
 
-      def __init__(self,x,y,couleur):
-            self.red=couleur[0]
-            self.green=couleur[1]
-            self.blue=couleur[2]
-            self.vertices = [[x,y,0],[x,y+1, 0],[x+1, y+1, 0],[x+1,y, 0]]
+    def __init__(self,x,y,couleur):
+        self.red=couleur[0]
+        self.green=couleur[1]
+        self.blue=couleur[2]
+        self.x = x
+        self.y = y
+    
+    def getVertices(self):
+        self.vertices = [[self.x,self.y,0],[self.x,self.y+1, 0],[self.x+1, self.y+1, 0],[self.x+1,self.y, 0]]
 
-      def draw(self):
-            glBegin(GL_QUADS)
-            glColor3f(self.red, self.green, self.blue)
-            for vertex in self.vertices:
-                  glVertex3f(vertex[0],vertex[1],vertex[2])
-            glEnd()
+    def draw(self):
+        self.getVertices()
+        glBegin(GL_QUADS)
+        glColor3f(self.red, self.green, self.blue)
+        for vertex in self.vertices:
+            glVertex3f(vertex[0],vertex[1],vertex[2])
+        glEnd()
 
 
 #Pour bouger tous les carres en meme temps, ils faut qu'ils partent tous du meme point, et ce point on doit y avoir acces facilement. Alors ce point c'est maintenant ton instance de ton objetc (appelons le mapiece) comme ceci : mapiece.Ax, meme principe pour le y...   (PAS UNDERSTAND, fin on le retrouve ou ?)
 
 
-class piece1(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[0,1,0]  # vert / S a droite
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color)]
-
-class piece2(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[1,0,0]  # rouge / S a gauche
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x-1,self.y-1,self.color),carre(self.x-1,self.y-2,self.color)]
-    
-class piece3(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[0,0,1]  # bleu / ligne
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x,self.y-3,self.color)]
-    
-class piece4(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[1,1,0]  # jaune / carre
-        self.list = [carre(self.x,self.y,self.color),carre(self.x+1,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color)]
-    
-class piece5(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[1,0,1]  # violet / T
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x,self.y-2,self.color)] 
-    
-class piece6(object):
-    def __init__(self):
-        self.x = 4 
-        self.y = 12
-        self.color =[0,1,0]  # cyan  / anti L
-        self.list = [carre(self.x+1,self.y,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color),carre(self.x,self.y-2,self.color)]
-    
-
 class piece(object):
     def __init__(self):
         self.x = 4 
         self.y = 12
-        self.color =[1,0.2,0.4]  # orange / L
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x+1,self.y-2,self.color)] 
-    
-    
+        index = random.randint(0,6)
+        ## et la c'est une liste des couleurs dans le meme ordre que les pieces !
+        colorList =[[1,0.2,0.4], [0,1,0], [1,0,1], [1,1,0], [0,0,1], [1,0,0], [0,1,0]] # orange / L
+        self.color = colorList[index]
+        #ca c'est la liste de toutes les pieces comme ca on peut faire un random choice dedans, puisque les methodes ci dessous sont les memes pour toutes les pieces !
+        totalList = [[carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x+1,self.y-2,self.color)],
+                [carre(self.x+1,self.y,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color),carre(self.x,self.y-2,self.color)],
+                    [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x,self.y-2,self.color)],
+                    [carre(self.x,self.y,self.color),carre(self.x+1,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color)],
+                    [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x,self.y-3,self.color)],
+                    [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x-1,self.y-1,self.color),carre(self.x-1,self.y-2,self.color)],
+                    [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color)]]
+        self.list = totalList[index]
     #ca c'est pour recuperer ta fameuse liste que tu avais au depart. Sauf que la en plus tu peux mettre les autres listes pour la rotation et faire un truc qui choisit la bonne :) ( LISTE TOUT EN HAUT ? OU DU COUP JPEUX METTRE MES OBJETS PIECES ? )
     ##Alors en fait, self.list, c'est la vraie liste de tes carres. Tu pourra meme y ajouter les listes de tes differentes configurartions, et comme ca en fait, tu fais la representation de ta piece dans ton code... c'est ca qui est dessine dans la methode draw(). Et non, tu ne peux pas mettre tes objets pieces, parce que jusqua ce que je dirai stop, c'est dans la classe piece, donc ca c'est la liste de cette piece en particulier...
     ##je crois que ca c'est toujours pas tres clair, alors si t'a des questions dessus n'hesite pas :)
+    ##mais du coup la ca marche et donc je sais pas si ca te va ou pas...
     
+    def myinit(self):# c'est la meme chose que le __init__ mais on peux pas appeler le init une deuxieme fois et on peut pas l'enlever, donc, ben voila...
+        self.x = 4
+        self.y = 12
+        index = random.randint(0,6)
+        ## et la c'est une liste des couleurs dans le meme ordre que les pieces !
+        colorList =[[1,0.2,0.4], [0,1,0], [1,0,1], [1,1,0], [0,0,1], [1,0,0], [0,1,0]] # orange / L
+        self.color = colorList[index]
+        #ca c'est la liste de toutes les pieces comme ca on peut faire un random choice dedans, puisque les methodes ci dessous sont les memes pour toutes les pieces !
+        totalList = [[carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x+1,self.y-2,self.color)],
+                         [carre(self.x+1,self.y,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color),carre(self.x,self.y-2,self.color)],
+                         [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x,self.y-2,self.color)],
+                         [carre(self.x,self.y,self.color),carre(self.x+1,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color)],
+                         [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x,self.y-3,self.color)],
+                         [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x-1,self.y-1,self.color),carre(self.x-1,self.y-2,self.color)],
+                         [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x+1,self.y-1,self.color),carre(self.x+1,self.y-2,self.color)]]
+        self.list = totalList[index]
+
+
     def getSquareList(self):
-        ## pour l'instant, on recalcule juste la liste, il faudra plus tard ajouter le choix de la bonne configuration et tout et tout
-        self.list = [carre(self.x,self.y,self.color),carre(self.x,self.y-1,self.color),carre(self.x,self.y-2,self.color),carre(self.x+1,self.y-2,self.color)]
+        ## alors maintenat on fait le choix de la piece a l'init de piece. Du coup on a une liste d'instances de carre. Alors il faut une boucle pour faire descendre tous les carres de la self.list, du coup cette methode ne sert plus a rien, tout se passe dans down. C'est d'ailleurs plus efficace de faire ca comme ca, parce qu'au lieu de recreer des instances de carre a chaque fois, on ne fait que changer leur parametre a chaque timer (move)
+        
         self.bas = "prout" #il faut aussi que ici tu mette un parametre a ton objet pour savoir ou est le bas... Comme ca tu compares ce parametre a ce qui representera le bas pour savoir si il est atteint ou pas..
     
     #cette methode descend ton objet, il faut l'appeler dans le timer... (JE L'AI APPELE DANS LE MOVE OU YA LE TIMER) ( MAIS VOILA YA DEUX MOVE ..)
     ##oui c'est ce qu'il fallait faire :)
     def down(self):
-        self.y -= 1
+        for thing in self.list:
+            thing.y-= 1
 
     #ca c'est pour bouger de gauche a droite. Il faut pourvoir le faire plus d'une fois par timer, donc on l'appelle a chaque fois qu'une touche est enfoncee. c'est a dire dans la fonction Keypressed ! ( LE PERMIER MOVE(SELF) )
     ##j'en ai marre et il fait chaud !!!!!!!!
@@ -168,7 +159,7 @@ class piece(object):
 
         #alors ici il faut que tu prenne le self.list rafraichit (c'est a dire apres avor appele la methode getSquareList, et que tu fasse une boucle pour appeler la methode draw de chaque element (les instances de la classe carre ) de ta liste.
         #donc vu qu'il me le faut pour faire des tests je te fais cette boucle, que tu n'aurais eu aucun mal a faire quoi qu'il arrive
-        self.getSquareList()
+        ##comme je l'ai dit ca ca sert plus a rien : self.getSquareList()
         for thing in self.list :
             thing.draw() #on appelle ici la methode draw de l'objet carre, et on s'en fout si on a 2 draw, parce que ils sont dans 2 classes differentes (la classe carre et la classe piece...)
 
@@ -200,18 +191,17 @@ def move():
     ## et vu que tu va jouer avec les x et y de cette instance, tu n'a plus besoin de tes 15000 variables globales qui sont en commmentaire audessus !
     ##voila donc a quoi ca va ressembler :
     
-    if pieceR.y>3 :
+    #if pieceR.y>3 :
         #j'ai enleve le truc du x, il est deja apelle dans le keyboardfunc...
         #mais ici on fait descendre la piece
-        pieceR.down()
-    
+    pieceR.down()
     ##tout simplement :) Mainetant tout le travail est fait a l'interieur de ta classe piece quelle qu'ell soit !
         
         ## ca je te laisse y reflechir encore un peu :)
     if pieceR.y<=3 : #ca ca veut dire que ta piece est arrivee en bas...
         #alors il va faloir gerer une representation dynamique de ce qu'est le bas. C'est a dire que une fois qu'on a commence a jouer le bas n'est plus juste une ligne...
         #donc moi je te propose de faire une nouvelle fonction par exemple bas(), que j'ai declaree plus haut, et qui va regarder si le pieceR.bas est sur la ligne du bas et si c'est le cas va faire en sorte d'incorporer cette piece qui vient d'atterir dans ce que tu choisira pour representer le bas, soit un objet, soit une liste ou ce que tu veux, mais choisit bien parce que ca va deja etre assez galere comme ca .... :( Du coup les ifs de cette fonction ne servent plus a grand chose, il faudra juste appeler ta nouvelle fonction bas avant de descendre ta piece..
-        test = 0#et donc pour l'instant je te mets ma super ligne qui ne sert a rien mais evite une erreur d'indentation a cause du if :
+        pieceR.myinit()##ca c'est parce au final, si la piece arrive en bas il faut en refaire une nouvelle !
 
 
 
@@ -320,7 +310,7 @@ for ligne in range(10):
 
 
 #alors maintenant avant d'appeler tes fonctions (move() et main()), il faut creer une premiere piece qui descend et qui est choisie aleatoirement :
-pieceR = randomPiece()
+pieceR = piece()
 
 
 move()
