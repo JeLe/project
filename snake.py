05228addfa7ae9590bd8246867718cba664857b7
JeLe <dev.lecarpentier@gmail.com>
2014-05-14 20:44:06 +0200

    bon git hub me dit conflit mais moi je vois pas...

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
 
-
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
@@ -15,16 +14,12 @@ snake=[]
 coord=[]
 snakeColor=[0.8,0.1,0.1]
 
-
 i=0
 j=0
 temps=0
 momentum=0
-taille = 5
+taille = 3
 direction = 'm'
-fi=random.randint(0,15)
-fj=-random.randint(0,7)
-foodcoord=[fi,fj]
 speed=2.5
 
 class carre(object):
@@ -32,7 +27,7 @@ class carre(object):
 		self.red=couleur[0]
 		self.green=couleur[1]
 		self.blue=couleur[2]
-		self.vertices = [[x,y,0],[x,y+0.99, 0],[x+0.99, y+0.99, 0],[x+0.99,y, 0]]
+		self.vertices = [[x,y,0],[x,y+1., 0],[x+1., y+1., 0],[x+1.,y, 0]]
 	def drawSquare(self):
 		glBegin(GL_QUADS)
 		glColor3f(self.red, self.green, self.blue)
@@ -40,13 +35,23 @@ class carre(object):
 			glVertex3f(vertex[0],vertex[1],vertex[2])
 		glEnd()
 
-food=carre(fi,fj,[1.,1.,0.0])
+
+def miam() :
+	
+	global foodcoord
+	i=random.randint(0,15)
+	j=-random.randint(0,7)
+	foodcoord=[i,j]
+	return(carre(i,j,[1.,1.,0.]))
+
+
+food=miam()
+
 def move():
 
 
-	global i,j,direction,snakeColor,temps,grid,taille,foodcoord,momentum,fi,fj,food,speed
-	u=1
-	threading.Timer(1/speed, move).start()
+	global i,j,direction,snakeColor,temps,grid,taille,foodcoord,momentum,food,speed
+        threading.Timer(1/speed, move).start()
 	temps+=1   #Sert pour ne pas tricher !
 			
 	if direction == 'm' :
@@ -59,8 +64,7 @@ def move():
 		j-=1
 
 	coord.append([i,j])
-
-
+        
 	if foodcoord==coord[len(snake)] :
 		taille+=1
 		if speed<4 :
@@ -70,6 +74,11 @@ def move():
 		else :
 			speed+=0.07
 		food=miam()
+                for item in coord :
+                        if item == foodcoord :
+                                food=miam()
+
+
 	if len(snake)>=taille :
 		snake.pop(0)
 		coord.pop(0)
@@ -84,21 +93,15 @@ def move():
 	snake.append(carre(i,j,snakeColor))
 
 	if i>15 or i<0 or j>0 or j<(-7):
-		reset()
+                reset()
 
-def miam() :
-	
-	global foodcoord
-	i=random.randint(0,15)
-	j=-random.randint(0,7)
-	foodcoord=[i,j]
-	return(carre(i,j,[1.,1.,0.]))
 
 def reset() :
 	global direction,i,j,taille,speed,momentum,temps
+
 	snake[:]=[]
 	coord[:]=[]
-	momentum=temps
+        momentum=temps
 	taille=3
 	speed=2.50
 	if direction=='k' :
@@ -141,7 +144,7 @@ def DrawGLScene():                        #La fonction dessin
 	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 	glLoadIdentity()
-	glTranslatef(-8., 3.069, -15.) 
+	glTranslatef(-8., 3.069, -10.) 
         global food
 	
 	
@@ -165,7 +168,7 @@ def keyPressed(key, x, y):
 
 
 	if key == ESCAPE or key == 'q':
-		stop
+		sys.exit()
 	if key == 'r' :
 		reset()
 	
@@ -217,4 +220,10 @@ for l in range(0,8) :
 		grid.append(carre(c,0-l,[0.1,0.15,0.53]))  #random.random()
 
 
+<<<<<<< HEAD
+#main() # ca c'est en comment uniquement pour pouvoir le mettre dans le projet complet !
+=======
 main()	
+
+		
+>>>>>>> FETCH_HEAD
